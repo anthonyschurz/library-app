aef9c7984a7a6b7efbc53a55cab85e9649dd60e7
anthonyschurz <anthony.schurz@gmail.com>
2016-06-19 12:31:12 -0700

    makes more changes

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -12,4 +12,15 @@ class UsersController < ApplicationController
     render :new
   end
 
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+    redirect_to "/users"
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
 end
--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 root to: "users#index"
 
 resources :users
+get "/sign_in", to: "sessions#new"
+
 end
